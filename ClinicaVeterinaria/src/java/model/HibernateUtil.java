71044bc35f09a99c5cc81e709639eb36b04f0284
adriarce10 <adriarce10@yahoo.com>
2017-06-22 10:55:55 -0400

    Nueva configuracion de HibernateUtil

--- ClinicaVeterinaria/src/java/model/HibernateUtil.java
+++ ClinicaVeterinaria/src/java/model/HibernateUtil.java
@@ -8,6 +8,9 @@ package model;
 
 import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
 /**
  * Hibernate Utility class with a convenient method to get Session Factory
@@ -17,22 +20,33 @@ import org.hibernate.SessionFactory;
  */
 public class HibernateUtil {
 
-    private static final SessionFactory sessionFactory;
-    
-    static {
-        try {
-            // Create the SessionFactory from standard (hibernate.cfg.xml) 
-            // config file.
-            //asi estaba la linea de abajo: sessionFactory = new AnnotationConfiguration().configure("/<default package>/hibernate.cfg.xml").buildSessionFactory();
-            sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
-        } catch (Throwable ex) {
-            // Log the exception. 
-            System.err.println("Initial SessionFactory creation failed." + ex);
-            throw new ExceptionInInitializerError(ex);
-        }
+    /**
+     * private static final SessionFactory sessionFactory;
+     *
+     * static { try { // Create the SessionFactory from standard
+     * (hibernate.cfg.xml) // config file. //asi estaba la linea de abajo:
+     * sessionFactory = new
+     * AnnotationConfiguration().configure("/<default package>/hibernate.cfg.xml").buildSessionFactory();
+     * sessionFactory = new
+     * AnnotationConfiguration().configure().buildSessionFactory(); } catch
+     * (Throwable ex) { // Log the exception. System.err.println("Initial
+     * SessionFactory creation failed." + ex); throw new
+     * ExceptionInInitializerError(ex); } }
+     *
+     * public static SessionFactory getSessionFactory() { return sessionFactory;
     }
-    
+     */
+    private static SessionFactory sessionFactory;
+
     public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) { // loads configuration and mappings 
+            Configuration configuration = new Configuration().configure();
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+
+            // builds a session factory from the service registry sessionFactory =
+            configuration.buildSessionFactory(serviceRegistry);
+        }
+
         return sessionFactory;
     }
 }
